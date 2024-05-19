bb3183c522ba54d0c36e28fe8228f6abc444b925
abed-37h <79279228+abed-37h@users.noreply.github.com>
2024-05-20 02:12:55 +0300

    Create page template

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
+import searchIcon from './assets/icons/magnify.svg';
+import cartIcon from './assets/icons/cart-outline.svg';
+import './styles/fonts.css';
 import './styles/reset.css';
 import './styles/style.css';
+
+window.addEventListener('load', () => {
+    const headerIcons = document.querySelector('.header-icons');
+
+    const search = new Image();
+    search.src = searchIcon;
+    search.alt = 'search';
+    search.className = 'icon search';
+
+    const cart = new Image();
+    cart.src = cartIcon;
+    cart.alt = 'cart';
+    cart.className = 'icon cart';
+
+    headerIcons.append(search, cart);
+});
 
